c78f78269d9a153247d548cd7afdd4d72be75c9d
s_harada <satoshi.harada@trustia.jp>
2019-04-09 17:15:56 +0900

    [gmg] [commit-message] [ProductCode Coding] アプリケーションの名前と機能の名前をLocalStorageに保存

--- src/components/CommitMessage.vue
+++ src/components/CommitMessage.vue
@@ -3,37 +3,48 @@
     <v-layout text-xs-center wrap>
       <v-flex xs12>
         <v-form ref="form" v-model="valid" lazy-validation>
-          <v-text-field v-model="appName"
+          <!-- アプリケーションの名前 -->
+          <v-combobox
+            v-model="appName"
+            :items="appNameItems"
             :rules="[v => !!v || 'アプリケーションの名前を指定してください。']"
+            prepend-icon="edit"
             label="アプリケーションの名前"
             required
-          ></v-text-field>
-
-          <v-text-field
+            @change="onChangeAppName($event)"
+          ></v-combobox>
+          <!-- 機能の名前 -->
+          <v-combobox
             v-model="funcName"
+            :items="funcNameItems"
             :rules="[v => !!v || '機能の名前を指定してください。']"
+            prepend-icon="edit"
             label="機能の名前"
             required
-          ></v-text-field>
-
+            @change="onChangeFuncName($event)"
+          ></v-combobox>
+          <!-- コミットの種類 -->
           <v-select
             v-model="commitType"
             :items="commitTypeItems"
             :rules="[v => !!v || 'コミットの種類を指定してください。']"
+            prepend-icon="edit"
             label="コミットの種類"
             required
           ></v-select>
-
+          <!-- コミットの概要 -->
           <v-text-field
             v-model="messageHeader"
-            :rules="[v => !!v || 'メッセージヘッダーを指定してください。']"
-            label="メッセージヘッダー"
+            :rules="[v => !!v || 'コミットの概要を指定してください。']"
+            prepend-icon="edit"
+            label="コミットの概要"
             required
           ></v-text-field>
-          
+          <!-- コミットの詳細 -->
           <v-textarea
             v-model="messageBody"
-            label="コミット内容の補足"
+            prepend-icon="edit"
+            label="コミットの詳細"
           >
           </v-textarea>
         </v-form>
@@ -54,16 +65,37 @@
     data: () => ({
       valid: true,
       appName: '',
+      appNameItems: [],
       funcName: '',
+      funcNameItems: [],
       commitType: '',
       commitTypeItems: [
-        'Test Coding',
-        'Product Coding',
-        'Refactoring'
+        'TestCode Coding',
+        'ProductCode Coding',
+        'Refactoring',
+        'Trial Coding'
       ],
       messageHeader: '',
       messageBody: '',
     }),
+    mounted() {
+      // set appNameItems
+      if (localStorage.getItem('app_name_items')) {
+        try {
+          this.appNameItems = JSON.parse(localStorage.getItem('app_name_items'))
+        } catch(e) {
+          localStorage.removeItem('app_name_items')
+        }
+      }
+      // set funcNameItems
+      if (localStorage.getItem('func_name_items')) {
+        try {
+          this.funcNameItems = JSON.parse(localStorage.getItem('func_name_items'))
+        } catch(e) {
+          localStorage.removeItem('func_name_items')
+        }
+      }
+    },
     computed: {
       commitMessage: function() {
         return (!this.appName ? "" : "[" + this.appName + "] ") +
@@ -78,6 +110,18 @@
     methods: {
       reset () {
         this.$refs.form.reset()
+      },
+      onChangeAppName(event) {
+        if (this.appNameItems.indexOf(event) === -1 && event != null) {
+          this.appNameItems.push(event)
+          localStorage.setItem('app_name_items', JSON.stringify(this.appNameItems))
+        }
+      },
+      onChangeFuncName(event) {
+        if (this.funcNameItems.indexOf(event) === -1 && event != null) {
+          this.funcNameItems.push(event)
+          localStorage.setItem('func_name_items', JSON.stringify(this.funcNameItems))
+        }
       },
     },
     components: {
